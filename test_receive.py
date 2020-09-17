70a6bee3ab25035828d48ccfc261cda45db5a012
Syrex-o <alberthenkel.ah@gmail.com>
2020-09-17 07:28:19 +0200

    Update test_receive.py

--- test_receive.py
+++ test_receive.py
@@ -16,18 +16,15 @@ radio = NRF24(GPIO, spidev.SpiDev())
 
 
 radio.begin(0, 17)
-time.sleep(1)
-
 radio.setRetries(15,15)
 
-radio.setPayloadSize(32)
+radio.setPayloadSize(3)
 radio.setChannel(10)
 
 radio.setDataRate(NRF24.BR_250KBPS)
 radio.setPALevel(NRF24.PA_MAX)
 
-radio.setAutoAck(1)
-radio.enableDynamicPayloads()
+radio.setAutoAck(True)
 radio.enableAckPayload()
 
 radio.openWritingPipe(pipes[1])
@@ -42,7 +39,7 @@ while True:
         while not radio.available():
                 time.sleep(1/100)
         recv_buffer = []
-        radio.read(recv_buffer, radio.getDynamicPayloadSize())
+        radio.read(recv_buffer, radio.getPayloadSize())
         print("Received: {}".format(recv_buffer))
         radio.stopListening()
         radio.write(recv_buffer)
